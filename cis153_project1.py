f2237eff33d08c3d01f1dd0f0678769eb0f2010b
文及信 Văn Cấp Tín (Victor) <62981175+dimsumenjoyer@users.noreply.github.com>
2023-09-27 13:31:35 -0400

    cis153_project1.py

--- cis153_project1.py
+++ cis153_project1.py
@@ -7,32 +7,49 @@ Professor Penta
 
 import random
 
-def fortune_teller():
-    question_input = input("Enter a question: ")
-    print("Question: " + question_input)
-    random_integer = random.randint(1, 6)
+def menu_options():
+  print("MENU OPTIONS: \n 1) Fortune Teller \n 2) Knock Knock Joke \n 3) Mad Lib")
+  done = False
+  while not done:
+    try:
+      user_input = int(input("Choose an activity: "))
+      if user_input == 1 or user_input == 2 or user_input == 3:
+        return user_input
+      else:
+        print("Error: Invalid Input. Please try again.")
+    except ValueError:
+      print("Error: Invalid Input. Please try again.")
 
-    if random_integer == 1:
+def random_integer():
+  randominteger = random.randint(1, 6)
+  return randominteger
+
+def fortune_teller(randominteger):
+    input("Enter a question: ")
+    randominteger = random_integer()
+    print(randominteger)
+    if randominteger == 1:
         print("Answer: Yes.")
-    elif random_integer == 2:
+    elif randominteger == 2:
         print("Answer: No.")
-    elif random_integer == 3:
+    elif randominteger == 3:
         print("Answer: Maybe I might think about considering it.")
-    elif random_integer == 4:
+    elif randominteger == 4:
         print("Answer: Try again later.")
-    elif random_integer == 5:
+    elif randominteger == 5:
         print("Awswer: No chance.")
-    elif random_integer == 6:
+    elif randominteger == 6:
         print("Awswer: Absolutely.")
+    return
 
 def knock_knock_joke():
     print("Knock. Knock.")
     done = False
     while not done:
-        user_input = input("Response: ")
-        if user_input != "Who's there?" and user_input != "Who is there?":
+        user_input2 = input("Response: ")
+        if user_input2 != "Who's there?" and user_input2 != "Who is there?":
             print("That is not how this works.")
-        elif user_input == "Who's there?" or user_input == "Who is there?":
+        elif user_input2 == "Who's there?" or user_input2 == "Who is there?":
             print("Boo - Boo. Who?")
             while not done:
                 user_input2 = input("Response: ")
@@ -43,56 +60,32 @@ def knock_knock_joke():
                     break
             done = True
             break
+            return
 
 def mad_lib():
   name = input("Choose a name: ")
   adjective = input("Choose an adjective: ")
   noun = input("Choose a noun: ")
   verb = input("Choose a verb: ")
-  print(name + " is " + adjective + " " + noun + " and " + verb + " their friends.")
-
-def mainmenu(menu_number):
-    if menu_number == 1:
-      print("FORTUNE TELLER: ")
-      return fortune_teller()
-    elif menu_number == 2:
-      print("KNOCK KNOCK JOKE: ")
-      return knock_knock_joke()
-    elif menu_number == 3:
-      print("MAD LIB: ")
-      return mad_lib()
-    else:
-      print("Error: Invalid Input.")
-'''
-# Test
-mainmenu(1)
-mainmenu(2)
-mainmenu(3)
-mainmenu("hi")
-'''
+  print(f"{name} is (a[n]) {adjective} {noun} and {verb} their friends.")
+  return
 
 def main_menu():
-  done = False
-  while not done:
-    print("ACTIVITY OPTIONS: \n 1) Fortune Teller \n 2) Knock Knock Joke \n 3) Mad lib")
-    try:
-      user_input4 = float(input("Choose a number to select your activity: "))
-      if user_input4 == 1:
-        print("FORTUNE TELLER: ")
-        return fortune_teller()
-        done = True
-        break
-      elif user_input4 == 2:
-        print("KNOCK KNOCK JOKE: ")
-        return knock_knock_joke()
-        done = True
-        break
-      elif user_input4 == 3:
-        print("MAD LIB: ")
-        done = True
-        break
-        return mad_lib()
-      else:
+    user_input = menu_options()
+    if user_input == 1:
+      print("FORTUNE TELLER: ")
+      fortune_teller(random_integer())
+    elif user_input == 2:
+      print("KNOCK KNOCK JOKE: ")
+      knock_knock_joke()
+    elif user_input == 3:
+      print("MAD LIB: ")
+      mad_lib()
+    else:
+      print("Error: Invalid Input.")
+    return
+
+main_menu()
         print("Error: Invalid input. Please try again.")
     except ValueError:
       print("Error: Invalid input. Please try again.")
