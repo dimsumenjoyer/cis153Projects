7b3569c5b7a486d96b22617a094ba012e66bbdd9
文及信 Văn Cấp Tín (Victor) <62981175+dimsumenjoyer@users.noreply.github.com>
2023-10-24 17:32:22 -0400

    Add files via upload

--- cis153project2.py
+++ cis153project2.py
@@ -7,8 +7,8 @@ Professor Penta
 
 import random
 
-# users_file = open("users.md", "w+")
-# log_file = open("log.md", "w+")
+users_file = open("users.md", "w+")
+log_file = open("log.md", "w+")
 
 def last_name():
     last_name = input("What is your surname?: ")
@@ -16,9 +16,11 @@ def last_name():
 
 def first_name():
     first_name = input("What is your given name?: ")
+    last_name()
     return first_name
 
 def generate_username():
+    first_name()
     random_number = random.randint(1000, 9999)
     given_name = first_name()
     surname = last_name()
@@ -26,7 +28,7 @@ def generate_username():
     username_lower = username.lower()
     return username_lower
 
-def getuserpassword():
+def create_userpassword():
     username = generate_username()
     users_file = open("users.md", "r+")
     user_file_data = users_file.read()
@@ -37,8 +39,13 @@ def getuserpassword():
         print("Error: User does not exist. Make a user.")
         return
 
+def create_account():
+    generate_username()
+    create_userpassword()
+    return
+
 def has_special_character():
-    password = getuserpassword()
+    password = create_userpassword()
     if "!" in password or "@" in password or "$" in password or "?" in password:
         print("Error: Passwords cannot contain special characters: !, @, $, ?.")
         return True
@@ -46,7 +53,7 @@ def has_special_character():
         return False 
 
 def validatepassword():
-    password = getuserpassword()
+    password = create_userpassword()
     password_length = len(password)
     if password_length < 5 and has_special_character():
         print(f"Error: Passwords must be 5 characters long. This password is {password_length} long and contains special characters: !, @, $, ?.")
@@ -55,7 +62,7 @@ def validatepassword():
     elif has_special_character():
         print("Error: This password has special characters: !, @, $, ?")
     else:
-        unencrypted_password = getuserpassword()
+        unencrypted_password = create_userpassword()
         return unencrypted_password
 
 def encryptpassword():
@@ -69,8 +76,8 @@ def correct_password():
         print("Welcome!")
         return True
     else:
+        print("Error: Incorrect password.")
         return False
-            
 
 def login_existing_user():
     done = False
@@ -86,6 +93,7 @@ def login_existing_user():
     return
 
 def review_log():
+    return
 
 def display_review_log():
     logfile = open("log_file")
@@ -100,14 +108,19 @@ def menu():
         user_input = int(input(("Choose an option: ")))
         done = False
         while not done:
-            existing_user = login_existing_user()
-            open_review_log = review_log()
             if user_input == 4:
                 print("Done!")
                 done = True
                 break
-            if user_input != [1,2,3] #1 and user_input != 2 and user_input != 3 and user_input != 4:
+            elif user_input != 1 and user_input != 2 and user_input != 3 and user_input != 4:
                 print("Error: Invalid input. Please try again.")
+            elif user_input == 1:
+                print("1) Login")
+                login_existing_user()
+            elif user_input == 2:
+                create_account()
+            elif user_input == 3:
+                display_review_log()
     except ValueError:
             print("Error: Invalid input. Please try again.")
     return
