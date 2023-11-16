6a90aee50e1129171ac032edaf5da7659dc872bd
文及信 Văn Cấp Tín (Victor) <62981175+dimsumenjoyer@users.noreply.github.com>
2023-11-16 15:33:24 -0500

    Add files via upload

--- cis153project3.py
+++ cis153project3.py
@@ -5,41 +5,71 @@ CIS153: Project 3
 11/1/2023, Due: 11/22/2023
 '''
 
-#example1 = {"requester": "resource": number_of_accessed}
-#example2 = {(requester, resource): number_of_accessed} 
+#example = {(requester, resource): number_of_accessed} 
 
 import re
 
 def log():
-    file = open("access_log.txt", "r")
-    print(file.read())   
-    return file
+    with open("access_log.txt", "r") as file:
+        fuck_regular_expressions = file.read().splitlines()
+    return fuck_regular_expressions
 
-#log()
+def parse_lines(fuck_regular_expressions):
+    find_requester_time_resource = "^(\S+) - - \[.+\] \"[A-Z]+ (\S+) "
+    find_stuff = re.compile(find_requester_time_resource)
+    requester_time_resource = {}
+    requesters = {} # {requester : number of requests they made}
+    resources = {} # {resource : number of requests for each resource}
+    for line in fuck_regular_expressions:
+        stuff = find_stuff.findall(line)
+        if len(stuff) > 0:
+            requester, resource = stuff[0]
+            if stuff[0] in requester_time_resource:
+                requester_time_resource[stuff[0]] += 1
+            else:
+                requester_time_resource[stuff[0]] = 1
+            if requester in requesters:
+                requesters[requester] += 1
+            else:
+                requesters[requester] = 1
+            if resource in resources:
+                resources[resource] += 1
+            else:
+                resources[resource] = 1
+    return requester_time_resource, requesters, resources
+
+def find_most_requested_resource(resources):
+    if resources:
+        most_requested_resource = max(resources, key = resources.get)
+        return most_requested_resource, resources[most_requested_resource]
+    else:
+        return None
+
+def find_most_active_requester(requesters):
+    if requesters:
+        most_requester = max(requesters, key = requesters.get)
+        return most_requester, requesters[most_requester]
+    else:
+        return None
+
+def find_requester_of_most_accessed_resource(requester_time_resource, most_accessed_resource):
+    most_requests = 0
+    most_requester = None
+    for (requester, resource), requests in requester_time_resource.items():
+        if resource == most_accessed_resource and requests > most_requests:
+            most_requests = requests
+            most_requester = requester
+    return most_requester, most_requests
 
 def find_requester_resource():
-    file = open("access_log.txt", "r")
-    read_file = file.readlines()
-    requester_time_resource = "^(\S+) - - \[.+\] \"[A-Z]+ (\S+) "
-    locate_stuff = re.compile(requester_time_resource)
-    requester = ""
-    resource = ""
-    requesters_resources = {}
-    for line in read_file:
-        stuff = locate_stuff.findall(line)
-        if len(stuff) > 0:
-            [(requester, resource)] = stuff
-            print(requester)
-            print(resource)
-    file.close()
+    file = log()
+    requester_time_resource, requesters, resources = parse_lines(file)
+    most_resource, request_count = find_most_requested_resource(resources)
+    requester_of_most_accessed_resource, requests = find_requester_of_most_accessed_resource(requester_time_resource, most_resource)
+    most_requester, request_count = find_most_active_requester(requesters)
+    print(f"Requester with most requests: {most_requester} - {request_count} requests.")
+    print(f"Requester's most requested resource: {requester_of_most_accessed_resource} - {requests} requests.")
+    print(f"Most resources: {most_resource} - {request_count} times.")
     return
 
 find_requester_resource()
-
-def resources():
-    resource = r'\"GET\s(.*?)\sHTTP\/1\.1\"\s(\d+)\s(\d+)'
-    return
-
-def requester_to_resources():
-    #example = {"requester", "resource", "number_of_accessed"}
-    return
