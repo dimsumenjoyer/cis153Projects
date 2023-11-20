31532559293cad36ab75862eb23bf97dc7933b2e
文及信 Văn Cấp Tín (Victor) <62981175+dimsumenjoyer@users.noreply.github.com>
2023-11-20 12:54:44 -0500

    Add files via upload

--- cis153project3.py
+++ cis153project3.py
@@ -11,7 +11,7 @@ import re
 
 '''
 def log():
-    with open("access_log.txt", "r") as file:
+
         find_requester_time_resource = "^(\S+) - - \[.+\] \"[A-Z]+ (\S+) "
         compiled_data = re.compile(find_requester_time_resource)
         for line in file:
@@ -20,34 +20,30 @@ def log():
     return data
 '''
 
-def log():
+def parse_lines():
     with open("access_log.txt", "r") as file:
-        data = tuple(file.read().splitlines()) # I don't know how to avoid using intermediate data structures to put it straight into dictionaries
-    return data
-
-def parse_lines(data):
-    find_requester_time_resource = "^(\S+) - - \[.+\] \"[A-Z]+ (\S+) "
-    find_stuff = re.compile(find_requester_time_resource)
-    requester_time_resource = {}
-    requesters = {} # {requester : number of requests they made}
-    resources = {} # {resource : number of requests for each resource}
-    for line in data:
-        stuff = find_stuff.findall(line)
-        if len(stuff) > 0:
-            requester, resource = stuff[0]
-            if stuff[0] in requester_time_resource:
-                requester_time_resource[stuff[0]] += 1
-            else:
-                requester_time_resource[stuff[0]] = 1
-            if requester in requesters:
-                requesters[requester] += 1
-            else:
-                requesters[requester] = 1
-            if resource in resources:
-                resources[resource] += 1
-            else:
-                resources[resource] = 1
-    return requester_time_resource, requesters, resources
+        find_requester_time_resource = "^(\S+) - - \[.+\] \"[A-Z]+ (\S+) "
+        requester_time_resource = {}
+        requesters = {} # {requester : number of requests they made}
+        resources = {} # {resource : number of requests for each resource}
+        for line in file:
+            find_stuff = re.compile(find_requester_time_resource)
+            stuff = find_stuff.findall(line)
+            if len(stuff) > 0:
+                requester, resource = stuff[0]
+                if stuff[0] in requester_time_resource:
+                    requester_time_resource[stuff[0]] += 1
+                else:
+                    requester_time_resource[stuff[0]] = 1
+                if requester in requesters:
+                    requesters[requester] += 1
+                else:
+                    requesters[requester] = 1
+                if resource in resources:
+                    resources[resource] += 1
+                else:
+                    resources[resource] = 1
+        return requester_time_resource, requesters, resources
 
 def find_most_requested_resource(resources):
     if resources:
@@ -73,8 +69,7 @@ def find_requester_of_most_accessed_resource(requester_time_resource, most_acces
     return most_requester, most_requests
 
 def find_requester_resource():
-    file = log()
-    requester_time_resource, requesters, resources = parse_lines(file)
+    requester_time_resource, requesters, resources = parse_lines()
     most_resource, request_count_of_most_requested_resource = find_most_requested_resource(resources)
     requester_of_most_accessed_resource, requests = find_requester_of_most_accessed_resource(requester_time_resource, most_resource)
     most_requester, request_count_from_most_requester = find_most_active_requester(requesters)
